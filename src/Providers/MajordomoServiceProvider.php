45cb113b96eef3a3956b9f40b30221131c4491ac
Chaos <870579887@qq.com>
2023-02-17 00:22:00 +0800

    feat: 调整迁移文件组织方式

--- src/Providers/MajordomoServiceProvider.php
+++ src/Providers/MajordomoServiceProvider.php
@@ -2,17 +2,55 @@
 
 namespace Chaos\Majordomo\Providers;
 
+use Chaos\Majordomo\Console\InstallCommand;
 use Illuminate\Support\ServiceProvider;
 
 class MajordomoServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../../config/majordomo.php' => config_path('majordomo.php'),
-        ]);
+        if ($this->app->runningInConsole()) {
+            $this->registerMigrations();
+
+            $this->commands([
+                InstallCommand::class,
+            ]);
+
+            $this->publishes([
+                __DIR__ . '/../../config/majordomo.php' => config_path('majordomo.php'),
+            ]);
+        }
+
         $this->loadRoutesFrom(__DIR__ . '/../router.php');
-        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+    }
+
+
+    private function registerMigrations()
+    {
+        $migrationsPath = __DIR__ . '/../../database/migrations/';
+
+        $items = [
+            'create_admins_table.php',
+            'create_menus_table.php',
+            'add_category_alias_to_permissions_table.php',
+            'create_routes_and_route_permissions_table.php'
+        ];
+
+        $paths = [];
+        foreach ($items as $key => $name) {
+            $paths[$migrationsPath . $name] = database_path('migrations') . "/" . $this->formatTimestamp($key + 1) . '_' . $name;
+        }
+
+        $this->publishes($paths, 'migrations');
+    }
+
+    /**
+     * @param $addition
+     * @return false|string
+     */
+    private function formatTimestamp($addition)
+    {
+        return date('Y_m_d_His', time() + $addition);
     }
 
     public function register()
